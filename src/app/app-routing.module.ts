174ca5134a9c393a295b92694a2a81af14aa8a61
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-05 12:09:26 +0300

    login and register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
+import { RegisterComponent } from './public/register/register.component';
+import { LoginComponent } from './public/login/login.component';
+import { PublicComponent } from './public/public.component';
 import { SecureComponent } from './secure/secure.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: SecureComponent}
+  {path: '', component: SecureComponent},
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
+    ]
+  }
 ];
 
 @NgModule({
