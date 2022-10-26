018378f302dbde1abf3b3b58f2b112df6be7e893
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-26 17:39:16 +0300

    new service method

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {environment} from "../../../environments/environment";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
   }
 
   ngOnInit(): void {
@@ -36,7 +36,7 @@ export class RegisterComponent implements OnInit {
 
   submit(): void {
     console.log(this.form.getRawValue());
-    this.http.post(`${environment.api}/register`, this.form.getRawValue())
+    this.authService.register(this.form.getRawValue())
       .subscribe(() => this.router.navigate(['/login']));
   }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${environment.api}/register`, data)
+  }
 }
