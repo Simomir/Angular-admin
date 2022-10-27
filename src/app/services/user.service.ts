ae5938974fe960d76a9353197fb6feb7a071246f
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-27 15:48:10 +0300

    new service method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  endpoint: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.endpoint = `${environment.api}/users`;
+  }
+
+  all() {
+    return this.http.get(this.endpoint);
+  }
 }
