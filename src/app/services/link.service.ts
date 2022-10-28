3b4d779f3764a15b3c75369834d366885acc9faa
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-28 12:11:21 +0300

    link service method

--- src/app/services/link.service.ts
+++ src/app/services/link.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+import { Link } from "../interfaces/link";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinkService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  all(id: number): Observable<Link[]> {
+    return this.http.get<Link[]>(`${environment.api}/users/${id}/links`)
+  }
 }
