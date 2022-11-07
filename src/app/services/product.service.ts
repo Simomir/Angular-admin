8053b64f3d86e38021a96a6553eaaca882a168ae
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-07 20:41:07 +0200

    product service logic

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Product } from "../interfaces/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  endpoint = `${environment.api}/products`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  all(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.endpoint)
+  }
 }
