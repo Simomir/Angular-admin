55e05f24fdc39d67c2be150261e09ba6face6be7
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-09 10:54:18 +0200

    displaying orders

--- src/app/secure/orders/orders.component.ts
+++ src/app/secure/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from "../../services/order.service";
 import { Order } from "../../interfaces/order";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons/faDollarSign";
 
 @Component({
   selector: 'app-orders',
@@ -9,6 +10,7 @@ import { Order } from "../../interfaces/order";
 })
 export class OrdersComponent implements OnInit {
   orders: Order[] = [];
+  dollarSign = faDollarSign;
 
   constructor(private orderService: OrderService) { }
 
